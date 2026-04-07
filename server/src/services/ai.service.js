cc8c3e6361922b963633219745f4cfbb8d84854f
2004vidya <pandey.vidya013@gmail.com>
2026-04-07 08:18:39 +0530

    created searchInternetTool and agent

--- server/src/services/ai.service.js
+++ server/src/services/ai.service.js
@@ -1,6 +1,8 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-import { HumanMessage, SystemMessage ,AIMessage} from "langchain";
+import { HumanMessage, SystemMessage ,AIMessage,tool,createAgent} from "langchain";
 import { ChatMistralAI } from "@langchain/mistralai";
+import * as z from "zod";
+import { searchInternet } from "./internet.service.js";
 
 const geminiModel = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash-lite",
@@ -12,7 +14,8 @@ const mistralModel = new ChatMistralAI({
   apiKey: process.env.MISTRAL_API_KEY,
 });
 export async function generateResponse(messages) {
-  const response = await geminiModel.invoke(messages.map((msg) => {
+  const response = await agent.invoke({
+    messages:messages.map((msg) => {
     if (msg.role === "user") {
       return new HumanMessage(msg.content);
     } else if (msg.role === "ai") {
@@ -20,10 +23,28 @@ export async function generateResponse(messages) {
     } else {
       return new SystemMessage(msg.content);
     }
-  }));
+  })
+  });
 
-  return response.text;
+  return response.messages[response.messages.length-1].text;
 }
+
+const searchInternetTool = tool(
+  searchInternet,
+  {
+    name:"search_internet",
+    description:"Search the internet for latest information",
+    schema:z.object({
+      query:z.string().describe("The query to search for")
+    })
+  }
+)
+
+const agent = createAgent({
+  model:geminiModel,
+  tools:[searchInternetTool],
+  systemMessage:"You are a helpful assistant that answers questions using the tools provided.If you need to search for up-to-date information on the internet, use the search_internet tool. If you have the answer in your knowledge base, use it."
+})
 
 export async function generateTitle(message) {
   const response = await mistralModel.invoke([
