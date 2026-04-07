a9e466b30446c737dfcf0fa2738919b348c8fb1c
2004vidya <pandey.vidya013@gmail.com>
2026-04-07 19:59:26 +0530

    deployment changes

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true }));
 app.use(morgan("dev"));
 
 app.use("/api/auth", authRoutes);
